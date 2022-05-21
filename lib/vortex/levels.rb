6f5b329a37525a0aa7bdcd8029f737530d3937ca
anim <anton@telpart.ru>
2022-05-21 04:14:55 +0300

    Level 0

--- lib/vortex/levels.rb
+++ lib/vortex/levels.rb
@@ -22,4 +22,36 @@ module Vortex
 
     def exec; end
   end
+
+  ##
+  # Level 0
+  class Level0 < LevelBase
+    LEVEL = 0
+
+    HOST = 'vortex.labs.overthewire.org'
+    PORT = 5842
+
+    def exec
+      @shell.puts("Connecting to #{HOST}:#{PORT}")
+      socket = TCPSocket.new(HOST, PORT)
+
+      @shell.puts('Reading data')
+      data = socket.read(4 * 4)
+      data = data.unpack('L4<')
+      data.each do |v|
+        @shell.puts(v)
+      end
+
+      response = data.inject(0) { |a, n| a += n }
+      @shell.puts("Sending response: #{response}")
+      response = [response].pack('L<')
+      socket.write(response)
+
+      @shell.puts('Reading credentials')
+      credentials = socket.read
+      @shell.puts(credentials)
+
+      socket.close
+    end
+  end
 end
